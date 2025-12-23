d807af3c52fe15266bc7c17477d5e1ff65b9d1be
Junior <eecfredes@gmail.com>
2025-12-22 21:11:50 -0300

    :rocket:Authentication section

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   # get "/products", to: "products#index"
 
   # get "/products/new", to: "products#new"
